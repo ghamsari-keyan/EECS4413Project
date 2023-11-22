f7be49438516270b855b0ff40f434f6ce5a719b1
yates-nikhil <nikhilyy@my.yorku.ca>
2023-11-22 15:52:47 -0500

    Update OrderItemDAOImpl.java

--- proj/src/main/java/dao/OrderItemDAOImpl.java
+++ proj/src/main/java/dao/OrderItemDAOImpl.java
@@ -7,7 +7,7 @@ import model.OrderItem;
 public class OrderItemDAOImpl {
 
 	/*
-	 * This class is used to get all order Items from a purchas
+	 * This class is used to get all order Items from a purchase
 	 * This will be a list containing (possibly) multiple items
 	 */
 	public List<OrderItem> getItemsFromPurchaseId(int purchId) {
