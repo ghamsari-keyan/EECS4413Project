11f47ecf53f4c49dfd42628bde8d43aaf3a1ee97
ghamsari-keyan <77293068+ghamsari-keyan@users.noreply.github.com>
2023-11-22 14:04:30 -0500

    added functionality to login links

---
+++ proj/src/main/java/controller/adminLogin.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class adminLogin
+ */
+@WebServlet("/adminLogin")
+public class adminLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public adminLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.getWriter().println("Successfully logged in");
+
+        // Redirect to the home servlet
+        response.sendRedirect(request.getContextPath() + "/books");
+	}
+
+}
--- proj/src/main/webapp/jsp/header.jsp
+++ proj/src/main/webapp/jsp/header.jsp
@@ -5,8 +5,8 @@
         <span style="text-align: center; vertical-align: middle; margin-bottom: 15px; margin-top: 15px; color: black"
             class="label">CPU4U</span> 
         <div style="text-align: right;">
-            <a href="/books">Customer Login |</a> 
-            <a href="/books">Admin Login</a>
+            <a href="${pageContext.request.contextPath}/sign-in.html">Customer Login |</a>             
+            <a href="${pageContext.request.contextPath}/sign-in-admin.html">Admin Login</a> 
         </div>
     </h2>
 </div>
