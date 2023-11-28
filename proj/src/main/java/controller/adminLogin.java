aa030d9c44770d91c7876eb2bad902d9ca5b2cf6
nikhilYates <nikhil.yates@outlook.com>
2023-11-28 18:20:06 -0500

    began admin processing and created admin dashboard jsp

--- proj/src/main/java/controller/adminLogin.java
+++ proj/src/main/java/controller/adminLogin.java
@@ -1,11 +1,17 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.AdministratorDAO;
+import dao.AdministratorDAOImpl;
+import dao.ItemDAOImpl;
 
 /**
  * Servlet implementation class adminLogin
@@ -13,6 +19,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/adminLogin")
 public class adminLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private AdministratorDAO adminDAO;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -20,6 +28,14 @@ public class adminLogin extends HttpServlet {
     public adminLogin() {
         super();
         // TODO Auto-generated constructor stub
+    }
+    
+    
+    
+    public void init(ServletConfig config) throws ServletException {
+    	super.init(config);
+    	
+    	adminDAO = new AdministratorDAOImpl();
     }
 
 	/**
@@ -38,10 +54,16 @@ public class adminLogin extends HttpServlet {
 		String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        response.getWriter().println("Successfully logged in");
+        
+        System.out.println(username);
+        System.out.println(password);
+        
+        
+        
+//        response.getWriter().println("Successfully logged in");
 
         // Redirect to the home servlet
-        response.sendRedirect(request.getContextPath() + "/books");
+//        response.sendRedirect(request.getContextPath() + "/books");
         
         
         
--- proj/src/main/java/dao/AdministratorDAOImpl.java
+++ proj/src/main/java/dao/AdministratorDAOImpl.java
@@ -1,10 +1,47 @@
 package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.*;
+
+import javax.servlet.ServletContext;
 
 import model.Administrator;
 
 public class AdministratorDAOImpl implements AdministratorDAO {
 
+	
+	private ServletContext servletContext;
+	
+	
+	public AdministratorDAOImpl(ServletContext servletContext) {
+		
+		this.servletContext = servletContext;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private Connection getConnection() throws SQLException {
+		String path = "jdbc:mysql://localhost:3306/computer_store";
+		return DriverManager.getConnection(path, "root", "EECS4413");
+	}
+
+	private void closeConnection(Connection connection) {
+		if (connection == null)
+			return;
+		try {
+			connection.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
 	/**
 	 * If we want to get a list of all the administrators
 	 */
